f54134cf79e23561708370cec4958c88e21f1449
Valto Silva <valtosilva7@gmail.com>
2018-10-27 00:27:52 -0400

    Updated  app/User.php

--- app/User.php
+++ app/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function posts()
     {
-        return $this->hasMany('App\Post', 'auth_id');
+        return $this->hasMany('App\Post', 'author_id');
     }
 
 
